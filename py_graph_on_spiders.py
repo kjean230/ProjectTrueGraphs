72ef5c7231bc531b02d4eaab8a5c8d5468b9569d
Kerwyn Jean <kjean2@fordham.edu>
2025-11-17 18:43:56 -0500

    fixed date time issue

--- py_graph_on_spiders.py
+++ py_graph_on_spiders.py
@@ -5,7 +5,13 @@ import pandas as pd
 import matplotlib as mpl
 import matplotlib.pyplot as plt
 from pathlib import Path
+import datetime
 
 CSV_FILE = Path(__file__).resolve().parent.parent / "ProjectTrueGraphs" / "csv files" / "file_of_spiders - Sheet1.csv"
-spider_df = pd.read_csv(CSV_FILE)
-print(spider_df.head(10))
+spider_df = pd.read_csv(CSV_FILE, dtype=str, )
+
+spider_df['year'] = spider_df['observed_on'].dt.year
+spider_df['month'] = spider_df['observed_on'].dt.month
+spider_df['date_month'] = spider_df['observed_on'].values.astype('datetime64[M]')
+
+print(spider_df[['observed_on', 'year', 'month', 'date_month']].head())
