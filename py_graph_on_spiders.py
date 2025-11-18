e394a40e9bb230a85f8f20228c4cb82420b520a6
Kerwyn Jean <kjean2@fordham.edu>
2025-11-17 19:24:37 -0500

    reverted changes in addColumn branch

--- py_graph_on_spiders.py
+++ py_graph_on_spiders.py
@@ -7,11 +7,26 @@ import matplotlib.pyplot as plt
 from pathlib import Path
 import datetime
 
+#creation of a path to the csv file and reading it into spider dataframe 
 CSV_FILE = Path(__file__).resolve().parent.parent / "ProjectTrueGraphs" / "csv files" / "file_of_spiders - Sheet1.csv"
-spider_df = pd.read_csv(CSV_FILE, dtype=str, )
+spider_df = pd.read_csv(CSV_FILE, dtype=str)
+# reading in the other two csv files into dataframes
+# reads the csv files for air quality and temperature/weather data
+CSV_FILE2 = Path(__file__).resolve().parent.parent / "ProjectTrueGraphs" / "csv files" / "file_of_monthly_weather.csv"
+weather_df = pd.read_csv(CSV_FILE2, dtype=str)
+CSV_FILE3 = Path(__file__).resolve().parent.parent / "ProjectTrueGraphs" / "csv files" / "file_of_air_quality.csv"
+air_quality_df = pd.read_csv(CSV_FILE3, dtype=str)
 
+# creating columns based on observed_on column in file_of_spiders.csv
+# using dt in pandas to extract year and month
+spider_df['observed_on'] = pd.to_datetime(spider_df['observed_on'])
 spider_df['year'] = spider_df['observed_on'].dt.year
 spider_df['month'] = spider_df['observed_on'].dt.month
 spider_df['date_month'] = spider_df['observed_on'].values.astype('datetime64[M]')
 
-print(spider_df[['observed_on', 'year', 'month', 'date_month']].head())
+spider_monthly = (
+    spider_df.groupby(['year', 'month', 'date_month'])
+    .size()
+    .reset_index(name='spider_count')   
+)
+print(spider_monthly.head())
