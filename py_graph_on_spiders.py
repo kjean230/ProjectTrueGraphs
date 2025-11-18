00febcda497ff0ec74021e50061770bdeed2e4b5
Kerwyn Jean <kjean2@fordham.edu>
2025-11-17 19:36:57 -0500

    created columns for two dataframes (air quality and temperature)

--- py_graph_on_spiders.py
+++ py_graph_on_spiders.py
@@ -10,6 +10,12 @@ import datetime
 #creation of a path to the csv file and reading it into spider dataframe 
 CSV_FILE = Path(__file__).resolve().parent.parent / "ProjectTrueGraphs" / "csv files" / "file_of_spiders - Sheet1.csv"
 spider_df = pd.read_csv(CSV_FILE, dtype=str)
+# reading in the other two csv files into dataframes
+# reads the csv files for air quality and temperature/weather data
+CSV_FILE2 = Path(__file__).resolve().parent.parent / "ProjectTrueGraphs" / "csv files" / "file_of_monthly_weather.csv"
+weather_df = pd.read_csv(CSV_FILE2, dtype=str)
+CSV_FILE3 = Path(__file__).resolve().parent.parent / "ProjectTrueGraphs" / "csv files" / "file_of_air_quality.csv"
+air_quality_df = pd.read_csv(CSV_FILE3, dtype=str)
 
 # creating columns based on observed_on column in file_of_spiders.csv
 # using dt in pandas to extract year and month
@@ -18,9 +24,19 @@ spider_df['year'] = spider_df['observed_on'].dt.year
 spider_df['month'] = spider_df['observed_on'].dt.month
 spider_df['date_month'] = spider_df['observed_on'].values.astype('datetime64[M]')
 
+# creating an aggregated dataframe that counts number of spiders observed per month
 spider_monthly = (
     spider_df.groupby(['year', 'month', 'date_month'])
     .size()
     .reset_index(name='spider_count')   
 )
-print(spider_monthly.head())
+
+# creating year and month columns in air quality dataframe
+air_quality_df['start_date'] = pd.to_datetime(air_quality_df['start_date'])
+air_quality_df['year'] = air_quality_df['start_date'].dt.year
+air_quality_df['month'] = air_quality_df['start_date'].dt.month
+
+# creating year and month columns in weather dataframe 
+weather_df['date_month'] = pd.to_datetime(weather_df['date_month'])
+weather_df['year'] = weather_df['date_month'].dt.year
+weather_df['month'] = weather_df['date_month'].dt.month
